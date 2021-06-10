381d77fa1c5d79b0b91708f43656b7291992316b
tina <tinawei@damaiapp.com.tw>
2021-06-10 18:32:42 +0800

    update post data in blog

---
+++ components/Home.js
@@ -0,0 +1,23 @@
+import Layout, { siteTitle } from './layout'
+export default function Home ({ allPostsData }) {
+    return (
+        <Layout home>
+      {/* Keep the existing code here */}
+      {/* Add this <section> tag below the existing <section> tag */}
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+    )
+ }
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
+import { getSortedPostsData } from '../lib/posts'
 export default function Home() {
   return (
     <Layout home>
@@ -15,4 +16,13 @@ export default function Home() {
       </section>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
