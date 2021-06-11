eadedf55b0b722778b2b404ecc4d3f8042b03026
tina <tinawei@damaiapp.com.tw>
2021-06-11 10:30:38 +0800

    add typescript

--- components/layout.tsx
+++ components/layout.tsx
@@ -4,23 +4,29 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'Ruofan wei'
-export const siteTitle = 'blog'
+const name = '[Your Name]'
+export const siteTitle = 'Next.js Sample Website'
 
-export default function Layout({ children, home }) {
-   return (
+export default function Layout({
+  children,
+  home
+}: {
+  children: React.ReactNode
+  home?: boolean
+}) {
+  return (
     <div className={styles.container}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="personal website using Next.js"
+          content="Learn how to build a personal website using Next.js"
         />
         <meta
           property="og:image"
           content={`https://og-image.vercel.app/${encodeURI(
             siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
@@ -33,21 +39,21 @@ export default function Layout({ children, home }) {
               src="/images/profile.jpg"
               className={utilStyles.borderCircle}
               height={144}
-              width={173}
+              width={144}
               alt={name}
             />
             <h1 className={utilStyles.heading2Xl}>{name}</h1>
           </>
         ) : (
           <>
-          <Link href="/">
+            <Link href="/">
               <a>
                 <Image
                   priority
                   src="/images/profile.jpg"
                   className={utilStyles.borderCircle}
-                  height={144}
-                  width={173}
+                  height={108}
+                  width={108}
                   alt={name}
                 />
               </a>
@@ -59,7 +65,7 @@ export default function Layout({ children, home }) {
             </h2>
           </>
         )}
-        </header>
+      </header>
       <main>{children}</main>
       {!home && (
         <div className={styles.backToHome}>
@@ -71,4 +77,3 @@ export default function Layout({ children, home }) {
     </div>
   )
 }
-
