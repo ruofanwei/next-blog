54d6706545b70dd83075c9e49bec6bf196f88406
tina <tinawei@damaiapp.com.tw>
2021-06-11 11:18:57 +0800

    add first post

--- components/layout.tsx
+++ components/layout.tsx
@@ -4,8 +4,8 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = '[Your Name]'
-export const siteTitle = 'Next.js Sample Website'
+const name = 'Ruofan wei'
+export const siteTitle = 'blog'
 
 export default function Layout({
   children,
@@ -39,7 +39,7 @@ export default function Layout({
               src="/images/profile.jpg"
               className={utilStyles.borderCircle}
               height={144}
-              width={144}
+              width={170}
               alt={name}
             />
             <h1 className={utilStyles.heading2Xl}>{name}</h1>
@@ -52,8 +52,8 @@ export default function Layout({
                   priority
                   src="/images/profile.jpg"
                   className={utilStyles.borderCircle}
-                  height={108}
-                  width={108}
+                  height={144}
+                  width={170}
                   alt={name}
                 />
               </a>
--- lib/posts.ts
+++ lib/posts.ts
@@ -8,20 +8,20 @@ import html from 'remark-html'
  * process.cwd()：獲得當前執行 node 指令時候的檔案夾目錄名
  **/
 const postsDirectory = path.join(process.cwd(), 'posts')
-console.log("postsDirectory",postsDirectory)
+//console.log("postsDirectory",postsDirectory)
 
 export function getSortedPostsData() {
     // Get file names under /posts
   const fileNames = fs.readdirSync(postsDirectory)
-  console.log("fileNames",fileNames) // [ 'first-post.js', 'pre-rendering.md', 'ssg-ssr.md' ]
+  // console.log("fileNames",fileNames) // [ 'first-post.js', 'pre-rendering.md', 'ssg-ssr.md' ]
   const allPostsData = fileNames.map(fileName => {
     // Remove ".md" from file name to get id
     const id = fileName.replace(/\.md$/, '')
     // Read markdown file as string
     const fullPath = path.join(postsDirectory, fileName)
-    console.log("fullPath",fullPath)
+    // console.log("fullPath",fullPath)
     const fileContents = fs.readFileSync(fullPath, 'utf8')
-    console.log("fileContents",fileContents)
+    // console.log("fileContents",fileContents)
     // Use gray-matter to parse the post metadata section
     const matterResult = matter(fileContents)
 
